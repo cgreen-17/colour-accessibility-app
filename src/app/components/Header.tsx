5ecb2768a12e0ea5dda5b9bfc84bd2aa5a81dd27
Chris Green <christopher.green1701@gmail.com>
2025-02-03 10:37:37 +0000

    Initial project setup with header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+      <header className="bg-gray-800 text-white p-4">
+        <h1 className="text-xl font-bold">Colour Accessibility App</h1>
+      </header>
+    );
+  }
+  
